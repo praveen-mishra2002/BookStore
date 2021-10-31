fa4aab5bfd61352ba42b7ba0988362e5a66d5541
Praveen Mishra <praveen.mishra@savigent.com>
2021-10-31 10:48:22 +0530

    Wrote the configuration and DB mapping classes

---
+++ src/BookStore.Infrastructure/Mappings/CategoryMapping.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using BookStore.Domain.Models;
+
+namespace BookStore.Infrastructure.Mappings
+{
+    public class CategoryMapping: IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(c => c.id);
+
+            builder.Property(c => c.Name)
+                .IsRequired()
+                .HasColumnType("varchar(150)");
+
+            builder.HasMany(c => c.Books)
+                .WithOne(b => b.Category)
+                .HasForeignKey(b => b.CategoryID);
+
+            builder.ToTable("Categories");
+                
+        }
+    }
+}
