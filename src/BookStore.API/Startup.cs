575bea0e2d97dc59e68de502ea7145fcea44bf08
Praveen Mishra <praveen.mishra@savigent.com>
2021-11-01 22:45:25 +0530

    Added Swagger and removed the default WeatherForecast file

--- src/BookStore.API/Startup.cs
+++ src/BookStore.API/Startup.cs
@@ -13,6 +13,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using BookStore.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using BookStore.API.Configuration;
 
 namespace BookStore.API
 {
@@ -33,11 +35,14 @@ namespace BookStore.API
             {
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddAutoMapper(typeof(Startup));
             services.AddControllers();
+            
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "BookStore.API", Version = "v1" });
             });
+            services.ResolveDependencies();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,9 +51,13 @@ namespace BookStore.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                /*
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BookStore.API v1"));
+                */
             }
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BookStore.API v1"));
 
             app.UseHttpsRedirection();
 
