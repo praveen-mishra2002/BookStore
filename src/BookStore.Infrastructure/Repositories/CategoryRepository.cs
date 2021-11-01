da9aff22447b31b7838692b7f7e2eae968ce97c8
Praveen Mishra <praveen.mishra@savigent.com>
2021-11-01 22:16:29 +0530

    Created Repository class for DB Context

---
+++ src/BookStore.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookStore.Domain.Interfaces;
+using BookStore.Domain.Models;
+using BookStore.Infrastructure.Context;
+
+namespace BookStore.Infrastructure.Repositories
+{
+    public class CategoryRepository: Repository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(BookStoreDbContext context) : base(context) { }
+    }
+}
